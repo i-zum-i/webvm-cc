1a734c83052444165580158521466333c09d058f
i-zum-i <izumi.no1@gmail.com>
2025-07-07 08:50:04 +0900

    初回テスト用にPUSH

---
+++ config.js
@@ -0,0 +1,8 @@
+WebVM.Config = {
+  diskImage: "disk.img",
+  container: document.getElementById("vm-container"),
+  autostart: true,
+  disableNetworking: false,
+  showToolbar: true,
+  bootCommand: "",  // 自動実行するコマンドがあればここに書く
+};
